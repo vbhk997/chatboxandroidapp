5cdf093c0bd3e66c4e6051ef276ebf1476015c91
vaibh <vaibhavaol201484@gmail.com>
2021-09-05 11:31:14 +0530

    second updated backup for chatting app

---
+++ app/src/main/java/com/example/inchat/Users.java
@@ -0,0 +1,32 @@
+package com.example.inchat;
+
+public class Users {
+
+    private String name;
+    private String email;
+    private String phonenum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+}
